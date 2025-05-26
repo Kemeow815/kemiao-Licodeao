039cebf5bf4f816853c019eba5f64997df81f337
Kemeow815 <kemiao@kmblog.icu>
2025-05-26 23:16:30 +0800

    添加react

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,15 @@ import rehypePrettyCode from "rehype-pretty-code";
 import sitemap from "@astrojs/sitemap";
 import { remarkOssImagePlugin } from "./src/utils/remarkOssImagePlugin";
 
+import react from "@astrojs/react";
+
 const options = {
   theme: "one-dark-pro",
 };
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [[rehypePrettyCode, options]],
